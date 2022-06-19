762bb1784373e344d12aa1304f274487e9700d4e
isagulovurmat <falone641234@gmail.com>
2022-06-19 22:10:06 +0600

    some func added

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var minuteLabel: UILabel!
     @IBOutlet weak var hourLabel: UILabel!
     
+    @IBOutlet weak var sliderTime: UISlider!
+    @IBOutlet weak var SegView: UISegmentedControl!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
@@ -21,7 +23,7 @@ class ViewController: UIViewController {
     var minutes: String = "00"
     var seconds: String = "00"
     var counter: Int = 0
-    
+    var isTimer = true
     var timer = Timer()
     
     override func viewDidLoad() {
@@ -30,6 +32,9 @@ class ViewController: UIViewController {
         hourLabel.text = hours
         minuteLabel.text = minutes
         secondLabel.text = seconds
+        view.backgroundColor = .lightGray
+        
+        sliderTime.isHidden = true
     }
     
     //MARK: Stop button action
@@ -58,10 +63,41 @@ class ViewController: UIViewController {
     
     //MARK: Play button action
     @IBAction func playButton(_ sender: UIButton) {
-        timer = Timer.scheduledTimer(timeInterval: 0.00001, target: self, selector: #selector(makeStep), userInfo: nil, repeats: true)
-        pauseButton.isEnabled = true
-        playButton.isEnabled = false
+        if isTimer == true{
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(makeStepBack), userInfo: nil, repeats: true)
+        }else{
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(makeStep), userInfo: nil, repeats: true)
+            pauseButton.isEnabled = true
+            playButton.isEnabled = false
+        }
     }
+    
+    @objc func makeStepBack(){
+        if counter != 0 {
+            if counter > 10{
+                secondLabel.text = "\(counter)"
+            }else{
+                secondLabel.text = "0\(counter)"
+            }
+            counter -= 1
+            sliderTime.value = Float(counter)
+            
+        }else{
+            playButton.isEnabled = false
+            stopButton.isEnabled = true
+            pauseButton.isEnabled = true
+            counter = 0
+            hours = "00"
+            minutes = "00"
+            seconds = "00"
+            secondLabel.text = seconds
+            minuteLabel.text = minutes
+            hourLabel.text = hours
+            timer.invalidate()
+        }
+    }
+    
+    
     
     @objc func makeStep(){
         
@@ -100,6 +136,30 @@ class ViewController: UIViewController {
         var sec = ""
         sec += String(format: "%02d", seconds)
         return [hour, min, sec]
+    }
+    
+    @IBAction func SegmentedTap(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex{
+        case 0:
+            view.backgroundColor = .lightGray
+            sliderTime.isHidden = true
+            isTimer = false
+        case 1:
+            view.backgroundColor = .white
+            sliderTime.isHidden = false
+            isTimer = true
+        default:
+            print("1")
+
+        }
+    }
+    
+    @IBAction func slider(_ sender: UISlider) {
+        
+        secondLabel.text = "\(Int(sender.value))"
+        counter = Int(sender.value)
+        
     }
     
 }
